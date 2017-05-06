467a0f1d70ef3046f4f783527459ac9da0473a5b
Mac－hc <mac-hc@MachcdeMacBook-Pro.local>
2017-05-06 09:42:23 +0800

    update

--- src/main/java/com/contact/common/MySessionListener.java
+++ src/main/java/com/contact/common/MySessionListener.java
@@ -3,9 +3,9 @@ package com.contact.common;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
+import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.phantomjs.MyPhantomJSDriver;
-
 
 public class MySessionListener implements HttpSessionListener {
 
@@ -15,8 +15,12 @@ public class MySessionListener implements HttpSessionListener {
 	public void sessionDestroyed(HttpSessionEvent se) {
 		String key = se.getSession().getId();
 		try {
-			WebDriver driver = new MyPhantomJSDriver(SessionUtils.getSessionId(key), 48105);
-			driver.quit();
+			String sessionId = SessionUtils.getSessionId(key);
+			if (!StringUtils.isEmpty(sessionId)) {
+				WebDriver driver = new MyPhantomJSDriver(sessionId, 48105);
+				driver.quit();
+			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
