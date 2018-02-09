584eaff129caf00587e80caf7f821e280ec19b4e
czh <948993066@qq.com>
2018-02-09 20:32:37 +0800

    fix https

--- src/main/resources/mobile/login.js
+++ src/main/resources/mobile/login.js
@@ -9,11 +9,13 @@ params.protocol = document.location.protocol;
 params.timestamp = new Date().getTime();
 jQuery.getJSON('/login.htm', params, function(jsonData) {
 	if (jsonData.code == "0000") {
-		window.location.href = jsonData.assertAcceptURL + "?backUrl=" + backUrl
-				+ "&artifact=" + jsonData.artifact;
+		// window.location.href = jsonData.assertAcceptURL + "?backUrl=" +
+		// backUrl
+		// + "&artifact=" + jsonData.artifact;
+		window.location.href = "https://shop.10086.cn/i/";
 		window.myData = "success";
 	} else if (jsonData.result == '9') {
-		window.location.href = "http://shop.10086.cn/i/";
+		window.location.href = "https://shop.10086.cn/i/";
 		window.myData = "success";
 	} else {
 		window.myData = jsonData.desc;
